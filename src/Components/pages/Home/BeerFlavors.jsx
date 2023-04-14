cb5c98c20300baf134d76e699bae0656215d9ab8
cerhiy99 <44707286+cerhiy99@users.noreply.github.com>
2023-04-14 03:17:50 +0300

    Update BeerFlavors.jsx

--- src/Components/pages/Home/BeerFlavors.jsx
+++ src/Components/pages/Home/BeerFlavors.jsx
@@ -4,13 +4,13 @@ import { CiBeerMugFull} from "react-icons/ci";
 const BeerFlavors = ({isMain}) => {
     const listBeer=[
         {name:"Budweiser",description: "популярне американське пиво з легким смаком та ароматом.", price:"130₴"},
-        {name:"Miller Lite",description: "світле пиво зі зниженим вмістом калорій та вуглеводів.", price:"100₴"},
-        {name:"Michelob Ultra",description: "світле пиво зі зниженим вмістом калорій та вуглеводів.", price:"90₴"},
-        {name:"Coors",description: "світле пиво зі зниженим вмістом калорій та вуглеводів.", price:"140₴"},
-        {name:"Corona",description: "світле пиво зі зниженим вмістом калорій та вуглеводів.", price:"65₴"},
-        {name:"Heineken",description: "світле пиво зі зниженим вмістом калорій та вуглеводів.", price:"200₴"},
-        {name:"Natural Light",description: "світле пиво зі зниженим вмістом калорій та вуглеводів.", price:"105₴"},
-        {name:"Busch",description: "світле пиво зі зниженим вмістом калорій та вуглеводів.", price:"85₴"},
+        {name:"Miller Lite",description: "Чорний, міцний, смажений, шоколадний, кавовий, оксамитовий.", price:"100₴"},
+        {name:"Michelob Ultra",description: "Цитрусово-смолистий, свіжий, гіркий, насичений, ароматний.", price:"90₴"},
+        {name:"Coors",description: "Солодовий, карамельний, пряний, дріжджовий, алкогольний, глибокий.", price:"140₴"},
+        {name:"Corona",description: "Кисло-солодке, фруктове, вишневе, легке, освіжаюче.", price:"65₴"},
+        {name:"Heineken",description: "Вишневий, дубовий, ванільний, темний, багатий, теплий.", price:"200₴"},
+        {name:"Natural Light",description: "Карамельний, медовий, солодкий, м'який, збалансований, кришталевий.", price:"105₴"},
+        {name:"Busch",description: "Кавовий, темний, міцний, ароматний, зігріваючий, насичений.", price:"85₴"},
     ]
     console.log(isMain)
   return (
